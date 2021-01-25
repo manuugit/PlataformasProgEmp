e0500b506d14318744e3f97d2752a6f369de3545
Manuela Hernández <manuela.hernandezr@upb.edu.co>
2021-01-24 22:20:38 -0500

    Prueba 1 galeria

--- js/login.js
+++ js/login.js
@@ -3,9 +3,11 @@ var ingresar = () =>{
  var usuario = document.getElementById("usuario").value;
  var password = document.getElementById("password").value;
  var datosUsuario = JSON.parse(localStorage.getItem('usuario'));
+ var linkGaleria = document.getElementById("linkGaleria");
 
  if(usuario == datosUsuario.usuario && password == datosUsuario.password){
      alert('Ingreso exitoso');
+     linkGaleria.click(); //redirecciona a galeria
  }
  else {
      alert('Los datos ingresados son incorrectos');
